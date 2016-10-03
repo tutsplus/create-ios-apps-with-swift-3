9c0284bb2f66dbfbf6a9b70edf476cb9fd913c15
Markus Mühlberger <markus@mmuehlberger.com>
2016-10-03 09:54:55 +0200

    Swift, Foundation and Other Useful Tips

---
+++ TutsplusFibonacci/TutsplusFibonacci-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  TutsplusFibonacci-Bridging-Header.h
+//  TutsplusFibonacci
+//
+//  Created by Markus Mühlberger on 03/10/2016.
+//  Copyright © 2016 Markus Mühlberger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
